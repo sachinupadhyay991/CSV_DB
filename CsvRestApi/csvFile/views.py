944d6c43c4c4190f63269743f129ba8ed996807e
sachinupadhyay991 <33253002+sachinupadhyay991@users.noreply.github.com>
2020-07-15 19:45:53 +0530

    Update views.py

--- CsvRestApi/csvFile/views.py
+++ CsvRestApi/csvFile/views.py
@@ -8,22 +8,4 @@ from .serializers import EmployeeSerializer
 class ViewList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-    '''
-    def get_csv(self):
-
-        queryset = Employee.objects.all()
-
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="export.csv"'
-
-        serializer = EmployeeSerializer(queryset, many=True)
-
-        header = EmployeeSerializer.Meta.fields
-
-        writer = csv.DictWriter(response, fieldnames=header)
-        writer.writeheader()
-        for row in serializer.data:
-            writer.writerow(row)
-
-        return response
-    '''
+    
